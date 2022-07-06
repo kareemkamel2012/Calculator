6b418a52d849db4fa880ea6efa2be9dc6a00faf0
Kareem Kamel <kamel024@umn.edu>
2022-07-06 18:36:32 -0500

    test

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,39 @@
+import java.util.Scanner;
+
 public class Calculator {
-    //test
+    private static double result;
+
+    public Calculator() {
+        this.result = 0;
+    }
+
+    public static void main(String[] args) {
+        char operation;
+        int num;
+
+        while (true) {
+            Scanner operationScanner = new Scanner(System.in);
+            System.out.println(result);
+            System.out.println("add, subtract, multiply, or divide?");
+            operation = operationScanner.nextLine().charAt(0);
+            System.out.println("How much?");
+            num = operationScanner.nextInt();
+            switch (operation) {
+                case 'a':
+                    result += num;
+                    break;
+                case 's':
+                    result -= num;
+                    break;
+                case 'm':
+                    result *= num;
+                    break;
+                case 'd':
+                    result /= num;
+                    break;
+                default:
+                    continue;
+            }
+        }
+    }
 }
